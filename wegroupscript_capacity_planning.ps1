7b94c84d44d4a8384b446f521c8bd1f568e1322b
Wisit Uamduang <mool3ank@live.com>
2023-10-12 21:10:19 +0700

    Update wegroupscript_capacity_planning.ps1

--- wegroupscript_capacity_planning.ps1
+++ wegroupscript_capacity_planning.ps1
@@ -5,23 +5,17 @@
     [string]$Password
 )
 
-# เชื่อมต่อกับ vCenter Server
 Connect-VIServer -Server $vCenterServer -User $Username -Password $Password
 
-# ดึงข้อมูลทุก Host
 $allHosts = Get-VMHost
-
-# ดึงข้อมูลทุก Datastore
 $allDatastores = Get-Datastore
 
-# สร้างออบเจ็กต์ CSV สำหรับแต่ละ Host และ Datastore
 $csvData = @()
 foreach ($VMhost in $allHosts) {
     foreach ($VMdatastore in $allDatastores) {
         $hostCapacity = $VMhost | Select-Object -Property Name, MemoryUsageGB, MemoryTotalGB, NumCpuCores, NumCpuThreads
         $datastoreCapacity = $VMdatastore | Select-Object -Property Name, CapacityGB, FreeSpaceGB
 
-        # สร้างออบเจ็กต์ CSV พร้อมเวลา
         $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
         $csvObject = New-Object PSObject -Property @{
             "Timestamp" = $timestamp
@@ -35,7 +29,6 @@ foreach ($VMhost in $allHosts) {
             "Free Space (GB)" = $datastoreCapacity.FreeSpaceGB
         }
 
-        # บันทึกออบเจ็กต์ CSV เป็นไฟล์ CSV แยกตาม Host และ Datastore
         $outputFile = Join-Path -Path $OutputPath -ChildPath "$($VMhost.Name)_$($VMdatastore.Name)_capacity_report.csv"
         $csvObject | Export-Csv -Path $outputFile -NoTypeInformation
 
@@ -43,13 +36,10 @@ foreach ($VMhost in $allHosts) {
     }
 }
 
-# ยกเลิกการเชื่อมต่อกับ vCenter Server
 Disconnect-VIServer -Server $vCenterServer -Confirm:$false
 
-# รวมไฟล์ CSV ทั้งหมดเป็นไฟล์ CSV เดียว
 $combinedCsvFile = Join-Path -Path $OutputPath -ChildPath "summary_capacity_report.csv"
 
-# ถ้ามีไฟล์ combined_capacity_report.csv อยู่แล้ว ให้รวมข้อมูลในไฟล์เดิมกับข้อมูลใหม่
 if (Test-Path $combinedCsvFile) {
     $existingCsvData = Import-Csv -Path $combinedCsvFile
     $combinedCsvData = $existingCsvData + $csvData
